2a73c5e24f25ebda0ee30542450ef4aa0de2f9e9
wxbntk <wubshett60@gmail.com>
2022-06-02 05:30:22 -0700

    Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
-int main(void) {
+
+/**
+
+ * main - Entry point
+
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+
+ * Return: 0
+
+ */
+
+
+int main(void)
+
+{
+
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+
+	return (0);
+
 }
