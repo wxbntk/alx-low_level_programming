9f869b62fe634803c673e3aa041969554c441067
wxbntk <wubshett60@gmail.com>
2022-06-05 13:08:28 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,30 +1,23 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
 
 /**
-
  * main - Entry block
-
  * Description: list single digit decimal
-
  * Return: always return to main
-
  */
 
 int main(void)
-
 {
 
-	char num;
+	int num;
 
-	for(num = '0'; num <= '9'; num++)
+	for (num = 0; num <= 9; num++)
 
 	{
 
-		putchar(num);
+		putchar(48 + num);
 
 	}
 
